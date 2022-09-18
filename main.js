8ec996cd63fe97fdf0d8ee35de34fd3b622b93d7
Olga Murashova <om.dreyfus@gmail.com>
2022-09-17 23:35:20 -0400

    animation

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ bodyEl.addEventListener("mousemove", (event)=>{
     const span = document.createElement("span");
     span.style.left = xPos + "px";
     span.style.top = yPos + "px";
+
+    const size = Math.random()*100;
+    span.style.width = size + "px";
+    span.style.height = size + "px";
+   
     bodyEl.appendChild(span);
+
+    setTimeout(()=>{span.remove();
+    }, 3000);
 
 })
